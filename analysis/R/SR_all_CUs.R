2c4a86c011dc799af0febd3a37e0e453c87f15be
DylanMG <dylan.glaser1@ucalgary.ca>
2024-12-13 14:46:30 -0800

    placeholder for fitting all CUs in a smart way

---
+++ analysis/R/SR_all_CUs.R
@@ -0,0 +1,13 @@
+# functional programming to loop through CU fits and diagnostics
+
+# load data
+
+# fit AR1
+
+# diagnostics
+
+# fit TV-alpha
+
+# diagnostics
+
+# wrangle important outputs and write them
