1a8e552b54f5dca3d76c7820ed69f8e70bd3e99d
DylanMG <dylan.glaser1@ucalgary.ca>
2025-02-12 10:51:18 -0800

    try to get process() working but keep hitting error

--- analysis/R/data_functions.R
+++ analysis/R/data_functions.R
@@ -45,9 +45,51 @@ get_Sgen <- function(a, b, int_lower, int_upper, Smsy){
 }
 
 #from BC's kusko code (https://github.com/brendanmichaelconnors/Kusko-harvest-diversity-tradeoffs/blob/master/functions.R#L237)
+
+#------------------------------------------------------------------------------#
+# Subsistence harvest control rule function
+#------------------------------------------------------------------------------#
+# sub < - subsistence requirement
+# egfloor <- escapement goal
+# run <- run size (i.e., pre-harvest recruitment) 
+# com <- maximum commercial harvest
+# for.error <- forecast error 
+# OU <- outcome uncertainty
+sub_hcr = function(sub, com, egfloor, run,for.error){
+  run.est <- run * rlnorm(1, 0, for.error); if(is.na(run.est)==TRUE){run.est <- run};if(is.na(run)==TRUE){run <- 0}
+  if(run.est - egfloor <= 0){hr = 0}
+  if(run.est > egfloor){
+    if((run.est - egfloor) <= (sub)){hr = (run - egfloor)/run}
+    if((run.est - egfloor) > (sub)){
+      if((run.est - egfloor) > (sub + com)){hr = (sub + com)/run}
+      if((run.est - egfloor) <= (sub + com)){hr = (sub + (run - egfloor - sub))/run}
+    }
+  }
+  if(hr < 0){hr=0}
+  if(hr >1 ){hr=1}
+  return(hr)
+}
+
+#------------------------------------------------------------------------------#
+# Status function (to estimate whether stock is overfished or predicted to go 
+#  extinct at a given harvest rate, over the long-term)
+#------------------------------------------------------------------------------#
+# U <- harvest rate
+# a <- productivity (Ricker a parameter)
+# b <- density dependence (Ricker beta parameter)
+SC.eq <- function(U,a,b){
+  a <- log(a)
+  S.eq <- max(0,(a-(-log(1-U)))/b)
+  C.eq <- max(0,((a-(-log(1-U)))/b)*exp(a-b*((a-(-log(1-U)))/b))-((a-(-log(1-U)))/b))
+  OF <- ifelse(U>0.5*a-0.07*a^2,1,0)
+  EX <- ifelse(S.eq==0,1,0)
+  return(c(S.eq,C.eq,OF,EX))
+}
+
+#take a slice of the posterior ---
 process.iteration = function(samp) {
   # 1.) extract names
-  nms = colnames(samp) ##DG switched from "names()"
+  nms = names(samp)
   A = 4
   ns = 8
   # 2.) extract elements according to the names and put them into the appropriate data structure
@@ -57,7 +99,7 @@ process.iteration = function(samp) {
   beta = unname(samp[substr(nms, 1, 4) == "beta"])
   last_resid = unname(samp[substr(nms, 1, 10) == "last_resid"])
   #phi = unname(samp["phi"])
-  Sigma_R = matrix(samp[substr(nms, 1, 7) == "Sigma_R"], ns, ns) #vcov needs to be infilled
+  #Sigma_R = matrix(samp[substr(nms, 1, 7) == "Sigma_R"], ns, ns) 
   pis = c(as.numeric(samp["pi_1"]), as.numeric(samp["pi_2"]), as.numeric(samp["pi_3"]), as.numeric(samp["pi_4"]))
   
   # states
@@ -68,9 +110,7 @@ process.iteration = function(samp) {
   output = list(
     alpha = alpha,
     beta = beta,
-    phi = phi,
     last_resid = last_resid,
-    Sigma_R = Sigma_R,
     S = S,
     R = R,
     pis = pis
@@ -78,5 +118,234 @@ process.iteration = function(samp) {
   
   # 4.) return output
   return(output)
+  
+}
+
+#------------------------------------------------------------------------------#
+# Generate deviations in alpha and beta for directional change simulations
+#------------------------------------------------------------------------------#
+# alpha <- median posterior estimate of alpha
+# beta <- median posterior estimate of beta
+# step.num <- number of time steps transition occurs over
+# start.step <- time step transition begins
+# ny <- number of years simulations runs over
+# n.pops <- number of pops
+SR_para_devs = function(alpha, beta, step.num, start.step, ny, n.pops){
+  id <- seq(1:n.pops)
+  
+  end.states.order <- c(1:n.pops) ##does this need to be ordered some funky way? thought it was just for plotting or something?
+  
+  alpha.end <- alpha[end.states.order]
+  beta.end <- beta[end.states.order]
+  
+  df <- data.frame(
+    alpha=vector(mode="numeric", length=2),
+    beta=vector(mode="numeric", length=2),
+    steps=vector(mode="numeric", length=2))
+  
+  input <- list(df,df,df,df,df,df,df,df)
+  
+  for(j in 1:n.pops) {                        
+    input[[j]]$steps<- c(1,step.num)
+    input[[j]]$alpha<- c(alpha[j]/alpha[j], alpha.end[j]/alpha[j]) ##WTF is going on here?! 
+    input[[j]]$beta<- c(beta[j]/beta[j], beta.end[j]/beta[j])		
+  }
+  output <- array(NA,dim=c(step.num,2,n.pops))
+  for (i in 1:length(input)) {
+    # in future; process 'n1' 2-at-a-time (1,2; 2,3; 3,4)
+    # not just 2-1,calling approx() each time and removing duplicates
+    n1 <- input[[i]]$steps[2]-input[[i]]$steps[1]+1
+    if(i!= step.num){if(n1>1) {
+      ans <- approx(
+        x=input[[i]]$alpha,
+        y=input[[i]]$beta,
+        method="linear",
+        n=n1
+      )
+      if(ans$x[1] < 1){ans$x <- rev(ans$x)
+      ans$y <- rev(ans$y)}
+      output[,,i] <- cbind(ans$x,ans$y)
+    }
+    }
+    if(i== step.num) {output[,,i] <- matrix(1, step.num,2)}   	
+  }
+  
+  SR_devs <- array(1,dim=c(ny,2,n.pops))
+  SR_devs[start.step:(start.step+step.num-1),,] <- output
+  
+  for(w in (start.step+step.num):ny){
+    SR_devs[w,,] <- output[step.num,,]
+  }
+  
+  return(SR_devs)	
+}
+
+#------------------------------------------------------------------------------#
+# Multi-stock simulation function with alternative structural forms
+#------------------------------------------------------------------------------#
+# ny <- the number of years
+# vcov.matrix <- process error variance-covariance matrix
+# phi <- the expected correlation through time
+# mat <- maturation schedule
+# alpha <- sub-stock productivity (NOT in log space)
+# beta <- sub-stock density dependence 
+# sub <- subsistence requirement
+# com <- maximum commercial harvest
+# egfloor <- escapement goal
+# pm.yr <- year of simulation that pms start to be calculated over
+# for.error <- forecast error (CV)
+# OU <- outcome uncertainty (CV)
+# Rec <- estimated recruits from last years of empirical data 
+# Spw <- estimated spawners from last years of empirical data
+# lst.resid <- estimated recruitment deviation from last year of empirical data
+# SR_rel <- structural form of the SR relationship ("Ricker" or "Beverton-Holt")
+# BH.alpha.CV <- magnitude (amplitude) of environmental forcing on alpha if SR_rel = "Beverton-Holt"
+# period <- period of enviro forcing cycle if SR_rel = "Beverton-Holt"
+# dir.SR <- flag for directional change in SR parameters ("Y" or "N")
+# SR.devs <- deviations in SR parameters by time step if dir.SR == "Y"
+# expan <- expansion of system to account for unmodelled spawning populations
+
+process = function(ny,vcov.matrix,phi,mat,alpha,beta,sub,com,egfloor,pm.yr,for.error,
+                   OU,Rec,Spw,lst.resid,SR_rel,BH.alpha.CV,period,dir.SR,SR_devs,expan){
+  ns <- length(alpha) #number of sub-stocks
+  for.error <- for.error
+  OU <- OU
+  m.alpha <- alpha
+  m.beta <- beta
+  
+  # create vectors of time varying alpha
+  if (SR_rel == "Beverton-Holt"){ 
+    beta.tim <- (alpha/beta)*exp(-1)
+    alpha.time <- matrix(NA,ny,length(alpha))
+    for (t in 1:ny){
+      alpha.time[t,] <- sin(2*pi*(t/period))*((alpha + (alpha * BH.alpha.CV)) - alpha) + alpha
+    }
+  }
+  
+  #Create recruitment deviations that are correlated among stocks 
+  epi <- rmvnorm(ny, sigma= vcov.matrix)
+  
+  #Build time series of Spawners (S), abundance of returning spawners pre-harvest
+  # (N), and the component of the residual that is correlated throught time (v)
+  R <- t(matrix(0,ns,ny))
+  S <- R * (1-0)
+  v <- R; v[,] <- 0
+  R[1:3,] <- Rec
+  N <- array(0,dim=c(ny,4,ns))
+  Ntot <- R; Ntot[,]<-0
+  H <- Ntot; S <- Ntot
+  S[4:7,] <- Spw
+  predR <- Ntot
+  
+  # populate first few years with realized states
+  R[4,] <- alpha[]*S[4,]*exp(-beta[]*S[4,]+(lst.resid)+epi[4,])
+  predR[4,] <- alpha[]*S[4,]*exp(-beta[]*S[4,])
+  v[4,] <- log(R[4,])-log(predR[4,])
+  v[v[,]=='NaN'] <- 0
+  
+  for(i in 5:7){
+    R[i,] <- alpha[]*S[i,]*exp(-beta[]*S[i,]+v[i-1,]+epi[i,])
+    predR[i,] <- alpha[]*S[i,]*exp(-beta[]*S[i,])
+    v[i,] <- log(R[i,])-log(predR[i,])
+    v[v[,]=='NaN'] <- 0	
+  }
+  
+  N[4:7,1,] <- R[4:7-(3),] * mat[1]
+  N[5:7,2,] <- R[5:7-(4),] * mat[2]
+  N[6:7,3,] <- R[6:7-(5),] * mat[3]
+  N[7,4,] <- R[7-(6),] * mat[4]
+  
+  # Loop through years of simulation	
+  for(i in (7+1):ny){
+    N[i,1,] <- R[i-(4),] * mat[1]
+    N[i,2,] <- R[i-(5),] * mat[2]
+    N[i,3,] <- R[i-(6),] * mat[3]
+    N[i,4,] <- R[i-(7),] * mat[4]
+    Ntot[i,] <- colSums(N[i,,])
+    
+    # apply harvest control rule
+    run.size <- sum(Ntot[i,])
+    if(is.na(run.size)==TRUE){run.size <- 0}
+    if(run.size > 999000) {run.size <- 1000000}
+    HR.all <- sub_hcr(sub,com,egfloor, run.size,for.error)
+    HR_adj <- 1
+    realized.HR <- (HR.all*HR_adj); realized.HR[realized.HR < 0] <- 0; realized.HR[realized.HR > 1] <-1
+    outcome_error <- (1+rnorm(1,0,OU))
+    H[i,] <- realized.HR*Ntot[i,]*ifelse(outcome_error<0, 0, outcome_error) 
+    S_exp <- Ntot[i,]-H[i,]
+    S_exp[S_exp<0] <- 0
+    S_exp[S_exp<50] <- 0
+    S[i,] <- S_exp
+    if (dir.SR == "T") {
+      alpha <- m.alpha* SR_devs[i,1,]
+      beta <- m.beta* SR_devs[i,2,]
+    }
+    
+    # predict recruitment
+    if (SR_rel == "Ricker"){
+      R[i,] <- alpha[]*S[i,]*exp(-beta[]*S[i,]+v[i-1,]+epi[i,]) ##add alpha.time? to here?
+      predR[i,] <- alpha[]*S[i,]*exp(-beta[]*S[i,])
+      v[i,] <- log(R[i,])-log(predR[i,])
+      v[v[,]=='NaN'] <- 0
+    }
+    
+    if (SR_rel == "Beverton-Holt"){
+      R[i,] = alpha.time[i,]*S[i,]/(1+(alpha.time[i,]/beta.tim[])*S[i,])*exp(phi*v[i-1,]+epi[i,])
+      predR[i,] = alpha.time[i,]*S[i,]/(1+(alpha.time[i,]/beta.tim[])*S[i,])
+      v[i,] = log(R[i,])-log(predR[i,])
+      v[v[,]=='NaN'] <- 0
+    }
+  }
+  
+  # Output
+  # Performance measures:
+  #	1: escapement
+  #	2: harvest
+  #	3: harvest rate
+  #	4: predicted overfished
+  #	5: predicted trending towards extinction
+  #	6: empirical extinction
+  #	7: proportion of years failed to meet subsistence goal
+  #	8: CV in harvest
+  #	9: proportion of tributary goals met
+  
+  pms <- matrix(NA,1,9) 
+  
+  S[S[,]=='NaN'] <- 0
+  Ntot[Ntot[,]=='NaN'] <- 0
+  over <- matrix(NA,length(alpha))
+  ext <- matrix(NA,length(alpha))
+  ext.emp <-ext
+  trib.gl <-ext
+  harvest_rate <- (H[pm.yr:ny,]/Ntot[pm.yr:ny,])[,1]
+  harvest_rate[harvest_rate>1] <- 1
+  harvest_rates <- (H[pm.yr:ny,]/Ntot[pm.yr:ny,])
+  harvest_rates[harvest_rates>1] <- 1
+  harvest_rate[harvest_rate=='NaN']<-1
+  harvest_rates[harvest_rates=='NaN']<-1
+  Smax <- round((m.alpha/m.beta)/m.alpha,digits=0)  
+  ln.alpha <- log(m.alpha)
+  Smsy <- round((ln.alpha*(0.5-0.07* ln.alpha))/m.beta)
+  for(j in 1:length(alpha)){
+    over[j] <- SC.eq(median(harvest_rates[,j]),alpha[j],beta[j])[3]
+    ext[j] <- SC.eq(median(harvest_rates[,j]),alpha[j],beta[j])[4]
+    ext.emp[j] <- ifelse(median(S[(ny-pm.yr):ny,j]) < ((log(alpha)/beta)*0.05)[j],1,0) # less than 5% of unfished biomass/abundance
+    trib.gl[j] <- ifelse(median(S[(ny-pm.yr):ny,j]) >= (Smsy[j]),1,0)
+  }
+  
+  pms[,1] <- (sum(S[pm.yr:ny,])/(ny - pm.yr +1)) * expan
+  pms[,2] <- (sum(H[pm.yr:ny,])/(ny - pm.yr +1)) * expan
+  pms[,3] <- median(harvest_rate)
+  pms[,4] <- sum(over)/length(alpha)
+  pms[,5] <- sum(ext)/length(alpha)
+  pms[,6] <- sum(ext.emp)/length(alpha)
+  pms[,7] <- sum(rowSums(H[pm.yr:ny,]) < (sub*0.90))/(ny - pm.yr +1)
+  pms[,8] <- sd(H[pm.yr:ny,])/mean(H[pm.yr:ny,]) 
+  pms[,9] <- sum(trib.gl)/length(alpha) 
+  
+  if (SR_rel == "Beverton-Holt"){
+    list(S=S[,],R=R[,], N=Ntot[,],H=H[,],BH_alpha = alpha.time, BH_beta = beta.tim, PMs=pms)}
+  else{
+    list(S=S[,],R=R[,], N=Ntot[,],H=H[,],PMs=pms)}
   
 }
--- analysis/R/fwd_sims.R
+++ analysis/R/fwd_sims.R
@@ -1,8 +1,13 @@
+#fwd sim attempt. "##" comments are Q's for BC
+
 library(here)
 library(tidyverse)
+library(mvtnorm) #for rmvnorm()
+
 source(here("analysis/R/data_functions.R"))
 
 #wrangle TVA fits into same structure as the "samps" matrix here(https://github.com/DylanMG/Kusko-harvest-diversity-tradeoffs/blob/master/load.R)
+#read models and posts
 TVA.fits <- lapply(list.files(here("analysis/data/generated/model_fits/TVA"), 
                               full.names = T), 
                    readRDS)
@@ -12,8 +17,7 @@ TVA.fits <- lapply(TVA.fits, rstan::extract)
 
 bench.posts <- read_rds(here("analysis/data/generated/benchmark_posteriors.rds"))
 
-#infilling the "samps" object from the kusko example, not I am NOT SURE if these need to be in order!
-  #- especially ordering on the things that will be matricies (i.e., Sigma_R, R, S)
+#infilling the "samps" object from the kusko example
 samps <- NULL
 pi.samps <- array(NA, dim = c(nrow(TVA.fits$MiddleYukonR.andtribs.$beta), A, length(TVA.fits)))
 p.samps <- array(NA, dim = c(nrow(TVA.fits$MiddleYukonR.andtribs.$beta), A, length(TVA.fits), 3))
@@ -51,200 +55,65 @@ colnames(median.pi.samps) <- paste0("pi_", 1:4)
 
 median.p.samps <- NULL
 for(i in 1:3){
-  median.p.samps <- cbind(median.p.samps, apply(p.samps[,,,i], c(1,2), median))
+  median.p.samps <- cbind(median.p.samps, apply(p.samps[,,,i], c(1,2), median)) 
 }
-median.p.samps <- median.p.samps[,  c(1,5,9, 2,6,10, 3,7,11, 4,8,12)] #rearrange to match kusko
+median.p.samps <- median.p.samps[,  c(1,5,9, 2,6,10, 3,7,11, 4,8,12)] #rearrange order of p match kusko
 colnames(median.p.samps) <- paste("p", (nyrs+1):nRyrs, rep(1:4, each=3), sep = "_")
 
 Sig.R <- cov(sig.R.samps)
 
-#old hack way to get Sig_R cols as posteriors to match kusko code
-if(FALSE){
-#get sigma_R varcov matrix 
-for(i in 1:length(names(TVA.fits))){
-  samps <- cbind(samps, TVA.fits[[i]]$sigma_R) #bind un-correlated sigma_R to samps
-}
-colnames(samps)[85:92] <- paste0("Sigma_R_", 1:8, "_", 1:8) #just the diagonal...
-
-#populate the off diagonal of the sigma_R "matrix"
-samps <- cbind(samps, matrix(0, nrow(samps), 8*8-8)) 
-
-colnames(samps)[93:ncol(samps)] #need to figure out how to name these MFers and if order matters
-
-paste0("Sigma_R_", 1:8, "_", rep(1:8, 8)) #hack to get all the sigma R names...
-  #^ could create full empty matrix of sigma_R's as 0's then populate them in a loop with i = i+8 or something
-} 
-
 #bind all samps into one object 
-samps.final <- cbind(samps, median.p.samps, median.pi.samps)
+samps <- cbind(samps, median.p.samps, median.pi.samps)
+
+#Set common conditions for simulations----------------------------------------------------
+num.sims = 50 # number of Monte Carlo trials #originally 500
+ny = 25 # number of years in forward simulation #originally 50
+pm.yr <- ny-20
+for.error <- 0.27 
+OU <- 0.1 
+
+# --- Create array to store outcomes ----------------------------------------------------
+harvest_goal <- seq(1000,100000,length.out=40)
+egfloor <- seq(1,100000,length.out=40)
+sim.outcomes <- array(NA,dim=c(length(egfloor),9, length(harvest_goal),num.sims)) ##why 9? length of perf metrics?
+sim.outcomes.spw.time <- array(NA,dim=c(ny,length(unique(sp_har$cu)),length(egfloor),length(harvest_goal),num.sims)) #was 13, changed to 8
 
 
-#------------------------------------------------------------------------------#
-# Multi-stock simulation function with alternative structural forms
-#------------------------------------------------------------------------------#
-# ny <- the number of years
-# vcov.matrix <- process error variance-covariance matrix
-# phi <- the expected correlation through time
-# mat <- maturation schedule
-# alpha <- sub-stock productivity (NOT in log space)
-# beta <- sub-stock density dependence 
-# sub <- subsistence requirement
-# com <- maximum commercial harvest
-# egfloor <- escapement goal
-# pm.yr <- year of simulation that pms start to be calculated over
-# for.error <- forecast error (CV)
-# OU <- outcome uncertainty (CV)
-# Rec <- estimated recruits from last years of empirical data 
-# Spw <- estimated spawners from last years of empirical data
-# lst.resid <- estimated recruitment deviation from last year of empirical data
-# SR_rel <- structural form of the SR relationship ("Ricker" or "Beverton-Holt")
-# BH.alpha.CV <- magnitude (amplitude) of environmental forcing on alpha if SR_rel = "Beverton-Holt"
-# period <- period of enviro forcing cycle if SR_rel = "Beverton-Holt"
-# dir.SR <- flag for directional change in SR parameters ("Y" or "N")
-# SR.devs <- deviations in SR parameters by time step if dir.SR == "Y"
-# expan <- expansion of system to account for unmodelled spawning populations
+## starting with this version
+# --- Time-varying Ricker SR dynamics ----------------------------------------------------
 
-process = function(ny,vcov.matrix,phi,mat,alpha,beta,sub,com,egfloor,pm.yr,for.error,
-                   OU,Rec,Spw,lst.resid,SR_rel,BH.alpha.CV,period,dir.SR,SR_devs,expan){
-  ns = length(alpha) #number of sub-stocks
-  for.error = for.error
-  OU = OU
-  m.alpha <- alpha
-  m.beta <- beta
-  
-  # create vectors of time varying alpha
-  if (SR_rel == "Beverton-Holt"){ 
-    beta.tim <- (alpha/beta)*exp(-1)
-    alpha.time <- matrix(NA,ny,length(alpha))
-    for (t in 1:ny){
-      alpha.time[t,] <- sin(2*pi*(t/period))*((alpha + (alpha * BH.alpha.CV)) - alpha) + alpha
+# set structural form of SR relationship
+SR_rel <-  "Ricker" 
+dir.SR <- "F" ##toggled to false because line below doesn't work
+SR_devs  <- SR_para_devs(apply(samps[,grepl("alpha", colnames(samps))],c(2),quantile,probs=c(0.5),na.rm=T),
+                         apply(samps[,grepl("beta", colnames(samps))],c(2),quantile,probs=c(0.5),na.rm=T),
+                         5,35,ny, length(unique(sp_har$cu))) #5 steps, starting at 35 - see function description
+##^can't figure out the point of this, approx() inside function doesn't even work in kusko 
+  #example (get out of bounds warning)
+
+# run simulations
+ptm <- proc.time()
+for (w in 1:length(harvest_goal)){
+  for (k in 1:length(egfloor)){
+    for (l in 1: num.sims){
+      draw <- sample(10000,1)
+      alpha <- process.iteration(samps[draw,])$alpha
+      beta <- process.iteration(samps[draw,])$beta
+      vcov.matrix <- Sig.R # had to change this, ##check w/ BC if appropriate to leave it fixed.
+      mat <- process.iteration(samps[draw,])$pis
+      Rec <- process.iteration(samps[draw,])$R
+      Spw <- process.iteration(samps[draw,])$S
+      lst.resid <- process.iteration(samps[draw,])$last_resid
+      sub <- ifelse(harvest_goal[w]<45000,harvest_goal[w],45000)
+      com <- ifelse(harvest_goal[w]<45000,0,harvest_goal[w]-45000)
+      expan <- 1/(rnorm(1,0.56,0.05))
+      
+      out <- process(ny,vcov.matrix,mat,alpha,beta,sub,com,egfloor[k],pm.yr,
+                     for.error,OU,Rec,Spw,lst.resid,SR_rel, dir.SR,
+                     #period,SR_devs, ##not needed because not modelling a BH or directional change?
+                     expan)
+      sim.outcomes[k,,w,l] <- out$PMs
+      sim.outcomes.spw.time[,,k,w,l] <- out$S
     }
-  }
-  
-  #Create recruitment deviations that are correlated among stocks 
-  epi = rmvnorm(ny, sigma= vcov.matrix)
-  
-  #Build time series of Spawners (S), abundance of returning spawners pre-harvest
-  # (N), and the component of the residual that is correlated throught time (v)
-  R = t(matrix(0,ns,ny))
-  S = R * (1-0)
-  v = R; v[,]=0
-  R[1:3,]=Rec
-  N = array(0,dim=c(ny,4,ns))
-  Ntot = R; Ntot[,]=0
-  H = Ntot; S = Ntot
-  S[4:7,] = Spw
-  predR = Ntot
-  
-  # populate first few years with realized states
-  R[4,] = alpha[]*S[4,]*exp(-beta[]*S[4,]+(phi*lst.resid)+epi[4,])
-  predR[4,] = alpha[]*S[4,]*exp(-beta[]*S[4,])
-  v[4,] = log(R[4,])-log(predR[4,])
-  v[v[,]=='NaN'] <- 0
-  
-  for(i in 5:7){
-    R[i,] = alpha[]*S[i,]*exp(-beta[]*S[i,]+phi*v[i-1,]+epi[i,])
-    predR[i,] = alpha[]*S[i,]*exp(-beta[]*S[i,])
-    v[i,] = log(R[i,])-log(predR[i,])
-    v[v[,]=='NaN'] <- 0	
-  }
-  
-  N[4:7,1,]=R[4:7-(3),] * mat[1]
-  N[5:7,2,]=R[5:7-(4),] * mat[2]
-  N[6:7,3,]=R[6:7-(5),] * mat[3]
-  N[7,4,]=R[7-(6),] * mat[4]
-  
-  # Loop through years of simulation	
-  for(i in (7+1):ny){
-    N[i,1,] = R[i-(4),] * mat[1]
-    N[i,2,] = R[i-(5),] * mat[2]
-    N[i,3,] = R[i-(6),] * mat[3]
-    N[i,4,] = R[i-(7),] * mat[4]
-    Ntot[i,] = colSums(N[i,,])
-    
-    # apply harvest control rule
-    run.size <- sum(Ntot[i,])
-    if(is.na(run.size)==TRUE){run.size <- 0}
-    if(run.size > 999000) {run.size <- 1000000}
-    HR.all = sub_hcr(sub,com,egfloor, run.size,for.error)
-    HR_adj = 1
-    realized.HR <- (HR.all*HR_adj); realized.HR[realized.HR < 0] <- 0; realized.HR[realized.HR > 1] <-1
-    outcome_error <- (1+rnorm(1,0,OU))
-    H[i,] =  realized.HR*Ntot[i,]*ifelse(outcome_error<0, 0, outcome_error) 
-    S_exp = Ntot[i,]-H[i,]
-    S_exp[S_exp<0] = 0
-    S_exp[S_exp<50] = 0
-    S[i,] = S_exp
-    if (dir.SR == "T") {
-      alpha <- m.alpha* SR_devs[i,1,]
-      beta <- m.beta* SR_devs[i,2,]
-    }
-    
-    # predict recruitment
-    if (SR_rel == "Ricker"){
-      R[i,] = alpha[]*S[i,]*exp(-beta[]*S[i,]+phi*v[i-1,]+epi[i,])
-      predR[i,] = alpha[]*S[i,]*exp(-beta[]*S[i,])
-      v[i,] = log(R[i,])-log(predR[i,])
-      v[v[,]=='NaN'] <- 0
-    }
-    
-    if (SR_rel == "Beverton-Holt"){
-      R[i,] = alpha.time[i,]*S[i,]/(1+(alpha.time[i,]/beta.tim[])*S[i,])*exp(phi*v[i-1,]+epi[i,])
-      predR[i,] = alpha.time[i,]*S[i,]/(1+(alpha.time[i,]/beta.tim[])*S[i,])
-      v[i,] = log(R[i,])-log(predR[i,])
-      v[v[,]=='NaN'] <- 0
-    }
-  }
-  
-  # Output
-  # Performance measures:
-  #	1: escapement
-  #	2: harvest
-  #	3: harvest rate
-  #	4: predicted overfished
-  #	5: predicted trending towards extinction
-  #	6: empirical extinction
-  #	7: proportion of years failed to meet subsistence goal
-  #	8: CV in harvest
-  #	9: proportion of tributary goals met
-  
-  pms <- matrix(NA,1,9) 
-  
-  S[S[,]=='NaN'] <- 0
-  Ntot[Ntot[,]=='NaN'] <- 0
-  over <- matrix(NA,length(alpha))
-  ext <- matrix(NA,length(alpha))
-  ext.emp <-ext
-  trib.gl <-ext
-  harvest_rate <- (H[pm.yr:ny,]/Ntot[pm.yr:ny,])[,1]
-  harvest_rate[harvest_rate>1] <- 1
-  harvest_rates <- (H[pm.yr:ny,]/Ntot[pm.yr:ny,])
-  harvest_rates[harvest_rates>1] <- 1
-  harvest_rate[harvest_rate=='NaN']<-1
-  harvest_rates[harvest_rates=='NaN']<-1
-  Smax <- round((m.alpha/m.beta)/m.alpha,digits=0)  
-  ln.alpha <- log(m.alpha)
-  Smsy <- round((ln.alpha*(0.5-0.07* ln.alpha))/m.beta)
-  for(j in 1:length(alpha)){
-    over[j] <- SC.eq(median(harvest_rates[,j]),alpha[j],beta[j])[3]
-    ext[j] <- SC.eq(median(harvest_rates[,j]),alpha[j],beta[j])[4]
-    ext.emp[j] <- ifelse(median(S[(ny-pm.yr):ny,j]) < ((log(alpha)/beta)*0.05)[j],1,0) # less than 5% of unfished biomass/abundance
-    trib.gl[j] <- ifelse(median(S[(ny-pm.yr):ny,j]) >= (Smsy[j]),1,0)
-  }
-  
-  pms[,1] <- (sum(S[pm.yr:ny,])/(ny - pm.yr +1)) * expan
-  pms[,2] <- (sum(H[pm.yr:ny,])/(ny - pm.yr +1)) * expan
-  pms[,3] <- median(harvest_rate)
-  pms[,4] <- sum(over)/length(alpha)
-  pms[,5] <- sum(ext)/length(alpha)
-  pms[,6] <- sum(ext.emp)/length(alpha)
-  pms[,7] <- sum(rowSums(H[pm.yr:ny,]) < (sub*0.90))/(ny - pm.yr +1)
-  pms[,8] <- sd(H[pm.yr:ny,])/mean(H[pm.yr:ny,]) 
-  pms[,9] <- sum(trib.gl)/length(alpha) 
-  
-  if (SR_rel == "Beverton-Holt"){
-    list(S=S[,],R=R[,], N=Ntot[,],H=H[,],BH_alpha = alpha.time, BH_beta = beta.tim, PMs=pms)}
-  else{
-    list(S=S[,],R=R[,], N=Ntot[,],H=H[,],PMs=pms)}
-  
+  }	
 }
