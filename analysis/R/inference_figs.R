dcf35dd126c94ca46fe9f4aaa48153432913ac0d
DylanMG <dylan.glaser1@ucalgary.ca>
2025-02-24 12:00:03 -0800

    add facetted figures summarising all CUs together

--- analysis/R/inference_figs.R
+++ analysis/R/inference_figs.R
@@ -1,11 +1,7 @@
 #old code for looping through and making individual plots 1 by 1
 library(here)
 library(tidyverse)
-library(ggplotify) #for as.ggplot() to help mcmc_combo() plotting
-library(rstan)
-library(bayesplot)
-library(shinystan)
-library(gsl) #for lambertw0() to calc U_MSY
+library(gsl)
 source(here("analysis/R/data_functions.R"))
 
 # read in model fits ---------------------------------------------------------------------
@@ -19,10 +15,15 @@ TVA.fits <- lapply(list.files(here("analysis/data/generated/model_fits/TVA"),
                    readRDS)
 names(TVA.fits) <- unique(sp_har$cu)[order(unique(sp_har$cu))]
 
-# processing data and fits to make plots later -------------------------------------------
-bench.par.table <- NULL 
+# process data and fits to make plots later ----------------------------------------------
+bench.par.table <- NULL #empty objects to rbind CU's outputs to 
 bench.posts <- NULL
+SR.preds <- NULL
+brood.all <- NULL
 a.yrs.all <- NULL
+AR1.resids <- NULL
+TV.resids <- NULL
+TV.SR.preds <- NULL
 
 for(i in unique(sp_har$cu)){
   sub_dat <- filter(sp_har, cu==i)
@@ -36,18 +37,20 @@ for(i in unique(sp_har$cu)){
     round(2)
   colnames(brood_t) <- c("BroodYear","S_lwr","S_med","S_upr","R_lwr","R_med","R_upr")
   
+  brood_t <- mutate(brood_t, CU = i)
+  
+  brood.all <- rbind(brood.all, brood_t)
+  
   #SR relationship based on full posterior---
   spw <- seq(0,max(brood_t$S_upr),length.out=100)
-  iter <- length(sub_pars$lnalpha)
-  SR_pred <- matrix(NA,length(spw), iter)
+  SR.pred <- matrix(NA,length(spw), length(sub_pars$lnalpha))
+  bench <- matrix(NA,length(sub_pars$lnalpha),5,
+                  dimnames = list(seq(1:length(sub_pars$lnalpha)), c("Sgen", "80.Smsy", "Umsy", "Seq", "S.recent")))
   
-  bench <- matrix(NA,iter,5,
-                  dimnames = list(seq(1:iter), c("Sgen", "80.Smsy", "Umsy", "Seq", "S.recent")))
-  
-  for(j in 1:iter){ 
+  for(j in 1:length(sub_pars$lnalpha)){ 
     ln_a <- sub_pars$lnalpha[j]
     b <- sub_pars$beta[j]
-    SR_pred[,j] <- (exp(ln_a)*spw*exp(-b*spw))
+    SR.pred[,j] <- (exp(ln_a)*spw*exp(-b*spw))
     
     bench[j,2] <- get_Smsy(ln_a, b) #S_MSY
     bench[j,1] <- get_Sgen(exp(ln_a),b,-1,1/b*2, bench[j,2]) #S_gen
@@ -56,9 +59,11 @@ for(i in unique(sp_har$cu)){
     bench[j,5] <- mean(sub_pars$S[j, (nyrs-4):nyrs]) #mean spawners in last generation 
   }
   
-  SR_pred <- as.data.frame(cbind(spw,t(apply(SR_pred, 1, quantile,probs=c(0.1,0.5,0.9), na.rm=T))))|>
-    round(2)
-  colnames(SR_pred) <- c("Spawn", "Rec_lwr","Rec_med","Rec_upr")
+  SR.pred <- as.data.frame(cbind(spw,t(apply(SR.pred, 1, quantile,probs=c(0.1,0.5,0.9), na.rm=T))))|>
+    round(2) |>
+    mutate(CU = i)
+  
+  SR.preds <- rbind(SR.preds, SR.pred)
   
   # get benchmarks & pars ------------------------------------------------------------------
   bench[,2] <- bench[,2]*0.8 #make it 80% Smsy
@@ -72,7 +77,7 @@ for(i in unique(sp_har$cu)){
   
   sub_benchmarks <- cbind(bench.quant, mean) |>
     as.data.frame() |>
-    mutate(cu = i) |>
+    mutate(CU = i) |>
     relocate('50%', 1)
   
   #other pars to report 
@@ -90,7 +95,7 @@ for(i in unique(sp_har$cu)){
   #summarise not other pars...
   par.summary <- filter(par.summary, row.names(par.summary) %in% c('lnalpha', 'beta',
                                                                    'sigma_R', 'phi')) |>
-    mutate(cu = i)
+    mutate(CU = i)
   par.summary[1,1] <- exp(par.summary[1,1]) #exp ln_alpha
   
   pars <- cbind(par.quants, par.summary)
@@ -103,15 +108,16 @@ for(i in unique(sp_har$cu)){
   
   bench.par.table <- bind_rows(bench.par.table, sub.bench.par.table)
   
-  #Plotting ------------------------------------------------------------------------------
-
   # then residuals---
   resid.quant <- apply(sub_pars$lnresid, 2, quantile, probs=c(0.1,0.25,0.5,0.75,0.9))[,(A):nRyrs] 
   
-  resids <- as.data.frame(cbind(sub_dat$year, t(resid.quant))) ##CHECK INDEX
-  colnames(resids) <- c("year","lwr","midlwr","mid","midupr","upr")
+  resids <- as.data.frame(cbind(sub_dat$year, t(resid.quant))) |>
+    mutate(CU = i)
+  colnames(resids) <- c("year","lwr","midlwr","mid","midupr","upr", "CU")
   
-  #time varying alpha plot 
+  AR1.resids <- rbind(AR1.resids, resids)
+  
+  #time varying alpha --------------------------------------------------------------------
   sub_pars_TVA <- rstan::extract(TVA.fits[[i]])
   
   a.yrs <- apply(exp(sub_pars_TVA$ln_alpha), 2, quantile, probs=c(0.1,0.5,0.9))
@@ -119,29 +125,121 @@ for(i in unique(sp_har$cu)){
   
   colnames(a.yrs) <- c("brood_year", "lwr", "mid", "upr")
   
-  
-  a.yrs.all <- rbind(a.yrs.all, data.frame(a.yrs, cu = i)) #store all alpha trends for plotting outside loop 
+  a.yrs.all <- rbind(a.yrs.all, data.frame(a.yrs, CU = i)) 
   
   #time varying alpha residuals  
   resid.quant <- apply(sub_pars_TVA$lnresid, 2, quantile, 
                        probs=c(0.1,0.25,0.5,0.75,0.9))[,(A):nRyrs]
   
-  resids <- as.data.frame(cbind(sub_dat$year, t(resid.quant))) 
-  colnames(resids) <- c("year","lwr","midlwr","mid","midupr","upr")
+  resids <- as.data.frame(cbind(sub_dat$year, t(resid.quant))) |>
+    mutate(CU = i)
+  TV.resids <- bind_rows(TV.resids, resids)
   
-
+  # median SR pred fit by year
+  for(j in 1:ncol(sub_pars_TVA$ln_alpha)){
+    ln_alpha_yr <- sub_pars_TVA$ln_alpha[,j]
+    pred <- data.frame(pred.R = (exp(median(ln_alpha_yr))*spw*exp(-median(sub_pars_TVA$beta)*spw))) |>
+      mutate(CU = i, 
+             year = unique(sp_har$year)[j]) |>
+      cbind(spw)
+    TV.SR.preds <- rbind(TV.SR.preds, pred)
+    }
 }
+colnames(SR.preds) <- c("Spawn", "Rec_lwr","Rec_med","Rec_upr", "CU")
+colnames(AR1.resids) <- c("year","lwr","midlwr","mid","midupr","upr", "CU")
+colnames(TV.resids) <- c("year","lwr","midlwr","mid","midupr","upr", "CU")
+
+# make key plots for pub -----------------------------------------------------------------
+
+ggplot() +
+  geom_abline(intercept = 0, slope = 1,col="dark grey") +
+  geom_ribbon(data = SR.preds, aes(x = Spawn/1000, ymin = Rec_lwr/1000, ymax = Rec_upr/1000),
+              fill = "grey80", alpha=0.5, linetype=2, colour="gray46") +
+  geom_errorbar(data = brood.all, aes(x= S_med/1000, y = R_med/1000, 
+                                      ymin = R_lwr/1000, ymax = R_upr/1000),
+                colour="grey", width=0, linewidth=0.3) +
+  geom_errorbarh(data = brood.all, aes(y = R_med/1000, xmin = S_lwr/1000, xmax = S_upr/1000),
+                 height=0, colour = "grey", linewidth = 0.3) +
+  geom_point(data = brood.all,
+             aes(x = S_med/1000,
+                 y = R_med/1000,
+                 color=BroodYear),
+             size = 1.5) +
+  geom_line(data = SR.preds, aes(x = Spawn/1000, y = Rec_med/1000)) +
+  facet_wrap(~CU, scales = "free") +
+  scale_colour_viridis_c(name = "Brood Year")+
+  labs(x = "Spawners (thousands)",
+       y = "Recruits (thousands)", 
+       title = "Spawner-recruit fits") +
+  theme(legend.position = "bottom",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.key.size = unit(0.4, "cm"),
+        legend.title = element_text(size=9),
+        legend.text = element_text(size=8))
+
+my.ggsave(here("analysis/plots/SR_fits.PNG"))
+
+ggplot(AR1.resids, aes(x=year, y = mid)) +
+  geom_ribbon(aes(ymin = lwr, ymax = upr),  fill = "darkgrey", alpha = 0.5) +
+  geom_ribbon(aes(ymin = midlwr, ymax = midupr),  fill = "black", alpha=0.2) +
+  geom_line(lwd = 1.1) +
+  coord_cartesian(ylim=c(-2,2)) +
+  labs(x = "Return year",
+       y = "Recruitment residuals", 
+       title = "AR1 recruitment residuals") + 
+  facet_wrap(~CU) +
+  theme(legend.position = "none",
+        panel.grid = element_blank()) +
+  geom_abline(intercept = 0, slope = 0, col = "dark grey", lty = 2)
+
+my.ggsave(here("analysis/plots/AR1_resids.PNG"))
+
+ggplot(TV.resids, aes(x=year, y = mid)) +
+  geom_ribbon(aes(ymin = lwr, ymax = upr),  fill = "darkgrey", alpha = 0.5) +
+  geom_ribbon(aes(ymin = midlwr, ymax = midupr),  fill = "black", alpha=0.2) +
+  geom_line(lwd = 1.1) +
+  labs(x = "Return year",
+       y = "Recruitment residuals", 
+       title = "Time-varying recruitment residuals") + 
+  facet_wrap(~CU) +
+  theme(legend.position = "none",
+        panel.grid = element_blank()) +
+  geom_abline(intercept = 0, slope = 0, col = "dark grey", lty = 2)
+
+my.ggsave(here("analysis/plots/TV_resids.PNG"))
 
 ggplot(a.yrs.all
-       |> filter(brood_year < 2018), aes(color = cu)) +
+       |> filter(brood_year < 2018), aes(color = CU)) +
   geom_line(aes(x = brood_year , y = mid), lwd = 2) +
   scale_color_viridis_d() +
   labs(y = "Productivity (Ricker alpha 80th percentiles)", x = "Brood year", 
-       title = "Time-varying productivity across CUs") #+
-# geom_hline(yintercept = c(2.51, 3.88), lwd = 1, lty = 2) #plugging in range of AR1 means
+       title = "Time-varying productivity across CUs")
 
 my.ggsave(here("analysis/plots/TVA_all.PNG"))
 
+ggplot() +
+  geom_point(data = brood.all,
+             aes(x = S_med/1000,
+                 y = R_med/1000),
+             size = 1.5) +
+  geom_line(data = TV.SR.preds, aes(x = spw/1000, y = pred.R/1000, color = year, group = year)) +
+  facet_wrap(~CU, scales = "free") +
+  scale_colour_viridis_c(name = "Brood Year")+
+  geom_abline(intercept = 0, slope = 1,col="dark grey") +
+  labs(x = "Spawners (thousands)",
+       y = "Recruits (thousands)", 
+       title = "Time varying productivity spawner-recruit fits") +
+  theme(legend.position = "bottom",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.key.size = unit(0.4, "cm"),
+        legend.title = element_text(size=9),
+        legend.text = element_text(size=8))
+
+my.ggsave(here("analysis/plots/TV_SR_fits.PNG"))
+
+# write important objects/tables to repo -------------------------------------------------
 write_rds(bench.posts, here("analysis/data/generated/benchmark_posteriors.rds")) 
 
 bench.par.table <- bench.par.table |>
