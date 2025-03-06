80a5ae886cebdddbc3e13b3275121b1353b2ffd1
DylanMG <dylan.glaser1@ucalgary.ca>
2025-03-05 16:27:48 -0800

    refit models and add new bench_par_table then build helper to compare with hannah's version to see if it's reproducible

---
+++ analysis/R/bench_compare.R
@@ -0,0 +1,5 @@
+bench.par1 <- read.csv(here("analysis/data/generated/bench_par_table.csv"))
+
+bench.par2 <- read.csv(here("analysis/data/generated/hannah_bench_par_table.csv"))
+
+cbind(bench.par1$bench.par, (bench.par1$mean - bench.par2$mean))
