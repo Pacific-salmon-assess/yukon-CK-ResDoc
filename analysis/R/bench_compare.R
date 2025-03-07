03e4fc1004f6245be664a7ab657eb74add65b543
DylanMG <dylan.glaser1@ucalgary.ca>
2025-03-07 11:24:24 -0800

    helper edits

--- analysis/R/bench_compare.R
+++ analysis/R/bench_compare.R
@@ -1,12 +1,13 @@
 bench.par1 <- read.csv(here("analysis/data/generated/bench_par_table.csv"))
-
 bench.par2 <- read.csv(here("analysis/data/generated/brendan_bench_par_table.csv"))
 bench.par3 <- read.csv(here("analysis/data/generated/hannah_bench_par_table.csv"))
 
 #Want all these to be TRUE
-all(bench.par1$CU == bench.par2$CU)
-all(bench.par1$bench.par == bench.par2$bench.par)
-all(bench.par1$mean == bench.par2$mean)
+all(bench.par1$CU == bench.par3$CU)
+all(bench.par1$bench.par == bench.par3$bench.par)
+all(bench.par1$mean == bench.par3$mean)
+
+cbind(bench.par1$CU, bench.par3$CU)
 
 #look at difference in means
 cbind(bench.par1$CU, bench.par1$bench.par,  (bench.par1$mean - bench.par2$mean))
