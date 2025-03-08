a746b17b2e969f3184ac75ca8719cb1eae18423b
DylanMG <dylan.glaser1@ucalgary.ca>
2025-03-08 08:43:59 -0800

    more sorting work

--- analysis/R/bench_compare.R
+++ analysis/R/bench_compare.R
@@ -2,12 +2,19 @@ bench.par1 <- read.csv(here("analysis/data/generated/bench_par_table.csv"))
 bench.par2 <- read.csv(here("analysis/data/generated/brendan_bench_par_table.csv"))
 bench.par3 <- read.csv(here("analysis/data/generated/hannah_bench_par_table.csv"))
 
-#Want all these to be TRUE
-all(bench.par1$CU == bench.par3$CU)
-all(bench.par1$bench.par == bench.par3$bench.par)
-all(bench.par1$mean == bench.par3$mean)
+#Want all these to be TRUE, run inside of all() to see WHICH dont match
+all(bench.par1$CU == bench.par2$CU)
+#if above is FALSE, which CUs are getting mixed up?
+cbind(bench.par1$CU, bench.par2$CU)[which(bench.par1$CU != bench.par2$CU),]
+
+all(bench.par1$bench.par == bench.par2$bench.par)
+all(bench.par1$mean == bench.par2$mean) #betas match
 
 cbind(bench.par1$CU, bench.par3$CU)
 
 #look at difference in means
 cbind(bench.par1$CU, bench.par1$bench.par,  (bench.par1$mean - bench.par2$mean))
+cbind(bench.par1$CU, bench.par2$CU)
+
+#is it just the order that is bessed up? sort means and compare
+cbind(sort(bench.par1$mean), sort(bench.par2$mean))
