41dc3dcc54215f647593d7d48c71ea9a0b6cd0af
hvhunter <hannah.hunter@dfo-mpo.gc.ca>
2025-03-10 10:47:48 -0700

    Remove duplicated lines

--- analysis/R/SR_fit.R
+++ analysis/R/SR_fit.R
@@ -15,12 +15,6 @@ if(refit == TRUE){
     
     sp_har1 <- filter(sp_har, CU == i) 
     
-    a_min <- 4
-    a_max <- 7 
-    nyrs <- nrow(sp_har1) #number of years of observations
-    A <- a_max - a_min + 1 #total age classes
-    nRyrs <- nyrs + A - 1 #number of recruitment years: unobserved age classes at ? to predict last year of spawners
-    
     stan.data <- list("nyrs" = nyrs,
                       "a_min" = a_min,
                       "a_max" = a_max,
