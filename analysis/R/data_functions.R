4f56736f2f34f494f44010a2f553ccf0e872064e
hvhunter <hannah.hunter@dfo-mpo.gc.ca>
2025-04-09 15:47:07 -0700

    fix 'alt rebuilding' HCR

--- analysis/R/data_functions.R
+++ analysis/R/data_functions.R
@@ -204,9 +204,9 @@ process = function(HCR,ny,vcov.matrix,mat,alpha,beta,pm.yr,for.error,OU,Rec,Spw,
       HR.all <- ER}
     if(HCR == "alt.rebuilding"){
       if(run.size <= 19000) catch <- 0
-      if(run.size >= 95000) catch <- run.size*0.4
-      if(run.size > 19000 & run.size < 95000){
-        dat <- data.frame(R=c(19000,95000), ER=c(0,0.4))
+      if(run.size >= 158333) catch <- run.size*0.4
+      if(run.size > 19000 & run.size < 158333){
+        dat <- data.frame(R=c(19000,158333), ER=c(0,0.4))
         lin <- lm(ER ~ R, data=dat)
         ER <- coef(lin)[1] + coef(lin)[2]*run.size
         catch <- run.size*ER
@@ -311,3 +311,76 @@ calcNeq <- function(Fmax)
   Seq_as <<- Neq_as * (1-U_as) # age/sex structured escapement
   sum(Neq_as)
 }
+
+
+
+# ------------------------------------------------------------------------------#
+# Simple simulation to visualize HCRs
+# ------------------------------------------------------------------------------#
+
+visualize_HCR <- function(HCRs, max_spwn=400000, int=1000) {
+  
+  out <- NULL
+  
+  for(j in HCRs){
+    HCR = j
+    
+    for(i in seq(1,max_spwn, int)) {
+      run.size = i
+      
+      if(is.na(run.size)==TRUE){run.size <- 0}
+      if(run.size > 999000) {run.size <- 1000000} 
+      if(HCR == "no.fishing"){HR.all <- 0}
+      if(HCR == "status.quo"){ 
+        catch <- ifelse(run.size<=42500, 0, run.size-42500)
+        HR.all <- ifelse(run.size==0, 0, catch/run.size)
+        if(HR.all > 0.8){       ## Add ER cap (80%) 
+          catch <- run.size*0.8
+          HR.all <- catch/run.size }}
+      if(HCR == "status.quo.cap"){
+        catch <- ifelse(run.size<=42500, 0, run.size-42500)
+        HR.all <- ifelse(run.size==0, 0, catch/run.size)
+        if(HR.all > 0.4){       ## Lower ER cap (40%) 
+          catch <- run.size*0.4
+          HR.all <- catch/run.size }}
+      if(HCR == "rebuilding"){
+        catch <- ifelse(run.size<=71000, 0, run.size-71000)
+        HR.all <- ifelse(run.size==0, 0, catch/run.size)
+        if(HR.all > 0.8){       ## ER cap (80%) 
+          catch <- run.size*0.8
+          HR.all <- catch/run.size }}
+      if(HCR == "rebuilding.cap"){
+        catch <- ifelse(run.size<=71000, 0, run.size-71000)
+        HR.all <- ifelse(run.size==0, 0, catch/run.size)
+        if(HR.all > 0.4){       ## lower ER cap (40%) 
+          catch <- run.size*0.4
+          HR.all <- catch/run.size }}
+      if(grepl("fixed.ER", HCR)){
+        ER <- ifelse(run.size==0, 0, as.numeric(gsub("\\D", "", HCR)))
+        catch <- run.size*ER
+        HR.all <- ER}
+      if(HCR == "alt.rebuilding"){
+        if(run.size <= 19000) catch <- 0
+        if(run.size >= 158333) catch <- run.size*0.4
+        if(run.size > 19000 & run.size < 158333){
+          dat <- data.frame(R=c(19000,158333), ER=c(0,0.4))
+          lin <- lm(ER ~ R, data=dat)
+          ER <- coef(lin)[1] + coef(lin)[2]*run.size
+          catch <- run.size*ER
+        }
+        HR.all <- catch/run.size
+      }
+      itn <- data.frame(HCR = HCR,
+                        run_size=run.size,
+                        HR = HR.all,
+                        catch=catch)
+      out <- bind_rows(out, itn)
+    }
+  }
+  return(out)
+}
+
+
+
+
+
