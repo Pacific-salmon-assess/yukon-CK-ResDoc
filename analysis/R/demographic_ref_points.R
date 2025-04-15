9cc524f3703f7c6c01ff8c84a90cd29842e9e34c
Connors <Brendan.Connors@dfo-mpo.gc.ca>
2025-04-15 12:59:46 -0700

    figure update

--- analysis/R/demographic_ref_points.R
+++ analysis/R/demographic_ref_points.R
@@ -198,7 +198,7 @@ ggplot(bench_post, aes(value, fill = unit, color = unit)) +
   geom_vline(xintercept = 1.5) +
   facet_wrap(~CU, scales = "free_y") +
   theme_sleek()+
-  xlab(expression(S[MSY])) +
+  xlab(expression(S[MSR])) +
   ylab("") +
   theme(legend.position = "bottom",
         axis.ticks.y = element_blank(), 
