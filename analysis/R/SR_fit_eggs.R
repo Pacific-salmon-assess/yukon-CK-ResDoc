e03a4675fd7f8b144a8ff3df166e7289c51e3f0d
Connors <brendan.connors@dfo-mpo.gc.ca>
2025-08-20 15:16:56 -0700

    Update SR_fit_eggs.R

--- analysis/R/SR_fit_eggs.R
+++ analysis/R/SR_fit_eggs.R
@@ -10,15 +10,15 @@ source(here("analysis/R/data_functions.R"))
 
 # fit AR1 models with egg mass as measure of reproductive output --------------------------------------
 for(i in unique(sp_har$CU)){
-    
-    sp_har1 <- filter(sp_har, CU == i) 
-    
+
+    sp_har1 <- filter(sp_har, CU == i)
+
     a_min <- 4
-    a_max <- 7 
+    a_max <- 7
     nyrs <- nrow(sp_har1) #number of years of observations
     A <- a_max - a_min + 1 #total age classes
     nRyrs <- nyrs + A - 1 #number of recruitment years: unobserved age classes at ? to predict last year of spawners
-    
+
     stan.data <- list("nyrs" = nyrs,
                       "a_min" = a_min,
                       "a_max" = a_max,
@@ -28,33 +28,33 @@ for(i in unique(sp_har$CU)){
                       "S_obs" = sp_har1$spwn,
                       "H_obs" = sp_har1$harv,
                       "S_cv" = sp_har1$spwn_cv,
-                      "H_cv" = sp_har1$harv_cv, 
-                      "Smax_p" = 0.75*max(sp_har1$spwn), #data for priors in semi_inform models, can tinker based on what assumed Smax is 
+                      "H_cv" = sp_har1$harv_cv,
+                      "Smax_p" = 0.75*max(sp_har1$spwn), #data for priors in semi_inform models, can tinker based on what assumed Smax is
                       "Smax_p_sig" = 0.75*max(sp_har1$spwn),
-                      "fem_S_comps"=fem_S_comps, 
+                      "fem_S_comps"=fem_S_comps,
                       "fem_fec_comps"=fem_fec_comps,
                       "fem_egg_mass_comps"=fem_egg_mass_comps)
-    
+
     AR1.eggs.fit <- stan(file = here("analysis/Stan/SS-SR_AR1_egg_mass.stan"),
                          data = stan.data,
                          cores = 4,
                          seed = 2,
                          iter = 2000)
-    
-    saveRDS(AR1.eggs.fit, here("analysis/data/generated/model_fits/AR1_egg_mass/", 
-                          paste0(i, "_AR1_eggs.rds")))    
-    
-    #TV.eggs.fit <- stan(file = here("analysis/Stan/SS-SR_TVA_egg_mass.stan"), 
-    #               data = stan.data,
-     #              cores = 4,
-      #             seed = 2,
-       #            iter = 2000,
-        #           control = list(adapt_delta = 0.999,
-         #                         max_treedepth = 20))
-    
-    
-    #saveRDS(TV.eggs.fit, here("analysis/data/generated/model_fits/TVA_egg_mass/", 
-     #                    paste0(i, "_TVA_egg_mass.rds")))
-    
-    
+
+    saveRDS(AR1.eggs.fit, here("analysis/data/generated/model_fits/AR1_egg_mass/",
+                          paste0(i, "_AR1_eggs.rds")))
+
+    TV.eggs.fit <- stan(file = here("analysis/Stan/SS-SR_TVA_egg_mass.stan"),
+                   data = stan.data,
+                   cores = 4,
+                   seed = 2,
+                   iter = 2000,
+                   control = list(adapt_delta = 0.999,
+                                  max_treedepth = 20))
+
+
+    saveRDS(TV.eggs.fit, here("analysis/data/generated/model_fits/TVA_egg_mass/",
+                         paste0(i, "_TVA_egg_mass.rds")))
+
+
 }
